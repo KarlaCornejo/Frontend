2470e90003cebd58e031cb154705de432352dfba
KarlaCornejo <131850074+KarlaCornejo@users.noreply.github.com>
2024-07-21 19:46:06 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentComponent } from './component/component.component';
+import { HeaderComponentComponent } from './component/header-component/header-component.component';
+import { FooterComponentComponent } from './component/footer-component/footer-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
